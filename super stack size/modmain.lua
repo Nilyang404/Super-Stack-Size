d417b3f5d09a926edfdce2dea2cf315e3860a75d
Neil CAO <63556313+Nilyang404@users.noreply.github.com>
2024-07-16 00:07:52 +1000

    Update modmain.lua

--- super stack size/modmain.lua
+++ super stack size/modmain.lua
@@ -5,8 +5,11 @@ local max_stackable_size = GetModConfigData("MAX_STACK_SIZE")
 local stack_creatures = GetModConfigData("STACK_CREATURES")
 local stack_spiders = GetModConfigData("STACK_SPIDERS")
 local stack_items = GetModConfigData("STACK_ITEMS")
+local Stackable = require("components/stackable")
 
-print("max_stackable_size: ", GetModConfigData("MAX_STACK_SIZE"))
+local orinigal_Put = Stackable.Get
+local orininal_Put = Stackable.Put
+
 -- stackable lists
 local creature_list = {"tallbirdegg_cracked", "tallbirdegg", "lavae_egg", "lavae_egg_cracked", "lavae_tooth",
                        "lavae_cocoon", "rabbit", "mole", "robin", "robin_winter", "bird_mutant", "crow", "puffin",
@@ -23,6 +26,7 @@ local spider_list = {"spider", "spider_healer", "spider_hider", "spider_moon", "
 local item_list = {"eyeturret_item", "shadowheart", "glommerwings", "deer_antler", "deer_antler1", "deer_antler2",
                    "deer_antler3", "klaussackkey", "portablecookpot_item", "portableblender_item", "portablespicer_item"}
 
+local blueprint_list = {"blueprint"}
 -- if stack size > 2^6 - 1 = 63, use new data type net_ushortint(0..65535) rather than net_smallbyte, or the server will crash client
 
 GLOBAL.TUNING.STACK_SIZE_LARGEITEM = max_stackable_size
@@ -76,6 +80,85 @@ local function TryAddStackableItem(prefab_name)
     end)
 end
 
+local function GetStackableBlueprint(num)
+    local num_to_get = num or 1
+    -- If we have more than one item in the stack
+    if self.stacksize > num_to_get then
+        local instance = SpawnPrefab( self.inst.prefab, self.inst.skinname, self.inst.skin_id, nil )
+        self:SetStackSize(self.stacksize - num_to_get)
+        instance.components.stackable:SetStackSize(num_to_get)
+        if self.ondestack ~= nil then
+            self.ondestack(instance, self.inst)
+        end
+        if instance.components.perishable ~= nil then
+            instance.components.perishable.perishremainingtime = self.inst.components.perishable.perishremainingtime
+        end
+    end
+end
+
+local function PutStackableBlueprint(item, source_pos)
+    assert(item ~= self, "cant stack on self" )
+    local ret
+    if item.prefab == self.inst.prefab and item.skinname == self.inst.skinname then
+
+        local num_to_add = item.components.stackable.stacksize
+        local newtotal = self.stacksize + num_to_add
+
+        local oldsize = self.stacksize
+        local newsize = math.min(self.maxsize, newtotal)
+        local numberadded = newsize - oldsize
+
+        if self.inst.components.perishable ~= nil then
+            self.inst.components.perishable:Dilute(numberadded, item.components.perishable.perishremainingtime)
+        end
+
+        if self.inst.components.inventoryitem ~= nil then
+            self.inst.components.inventoryitem:DiluteMoisture(item, numberadded)
+        end
+
+        if self.inst.components.edible ~= nil then
+            self.inst.components.edible:DiluteChill(item, numberadded)
+        end
+
+        if self.inst.components.curseditem ~= nil then
+            self.inst.skipspeech = true
+        end
+
+        if self.maxsize >= newtotal then
+            item:Remove()
+        else
+            _src_pos = source_pos
+            item.components.stackable.stacksize = newtotal - self.maxsize
+            _src_pos = nil
+            item:PushEvent("stacksizechange", {stacksize = item.components.stackable.stacksize, oldstacksize=num_to_add, src_pos = source_pos })
+            ret = item
+        end
+
+        _src_pos = source_pos
+		self.stacksize = math.min(newsize, MAXUINT)
+        _src_pos = nil
+        self.inst:PushEvent("stacksizechange", {stacksize = self.stacksize, oldstacksize=oldsize, src_pos = source_pos})
+    end
+    return ret
+end
+
+
+
+local function TryAddStackableBluePrint(prefab_name)
+    AddPrefabPostInit(prefab_name, function(inst)
+        if inst.components.inventoryitem ~= nil then
+            if (inst.components.stackable == nil) then
+                inst:AddComponent("stackable")
+            end
+            if inst.components.teacher then
+                inst.components.stackable.Get = GetStackableBlueprint
+                inst.components.stackable.Put = PutStackableBlueprint
+            end
+                
+        end
+    end)
+end
+
 local function handle_error(err)
     return "Caught an error: " .. err
 end
@@ -92,6 +175,11 @@ local function AddStackableItem(item_list)
     end
 end
 
+local function AddStackableBlueprint(blueprint_list)
+    for key, prefab_name in pairs(blueprint_list) do
+        TryAddStackableBluePrint(prefab_name)
+    end
+end
 
 if IsServer then
     if stack_creatures == true then
@@ -103,4 +191,5 @@ if IsServer then
     if stack_items == true then
         AddStackableItem(item_list)
     end
+    -- AddStackableBlueprint(blueprint_list)
 end
